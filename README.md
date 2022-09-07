55a4c0ab337a00990c708ce56071efb65c16ec06
Pedro Victor <pedro20002victor@gmail.com>
2022-09-07 17:51:52 -0300

    Conversao de tipos

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Estudos Dart
 - Modulo 1
    + Conhecimento sobre tipos INT e DOUBLE
+   + Tipo String 
    + Conteudo parecido com C#
-   
+   + Conversao de int para String: {
+      int.parse();
+      double.parse();
+      Number + toString();
+      toStringAsFixed(); double to String
+   }
+
--- bin/primeiro_projeto.dart
+++ bin/primeiro_projeto.dart
@@ -2,11 +2,28 @@
 void main() {
   int age = 20;
   int age2 = 20;
+  
   double height = 1.75;
-  bool geek = true;
+  double weight = 70.2;
+
   bool teste = (age == age2);
 
-  print('Age $age');
-  print('Altura $height');
-  print(teste);
+  String name = "Pedro";
+  String surname = "Borges";
+
+  // Conversion
+  var n1 = int.parse('10');
+  var n2 = double.parse("10");
+  var n3 = 90.toString();
+  var n4 = 190.toStringAsFixed(2);
+  
+
+  print(
+    "Username: $name\n"
+    "Age: $age\n"
+    "IMC: $height $weight\n"
+    "---------------\n"
+    "Surname: $surname"
+  );
+  print("$n1 $n2 $n3 $n4");
 }
